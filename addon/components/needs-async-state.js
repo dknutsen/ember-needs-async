674d05bf635ce2a27033dbbb648e133c00eebf9f
Dan Knutsen <dknutsen@gmail.com>
2019-06-23 21:02:59 -0500

    Make test pass - empty DS.ManyArray correctly yields loaded state

--- addon/components/needs-async-state.js
+++ addon/components/needs-async-state.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
+import { computed, get } from '@ember/object';
 import layout from '../templates/components/needs-async-state';
 
 export default Component.extend({
   layout,
-  tagName: ''
+  tagName: '',
+  isState: computed('taskInstance.{isRunning,value,error}', 'state', function() {
+    return !!get(this.taskInstance, this.state);
+  })
 });
